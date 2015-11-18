ee263fb4f72c44d996d5588f2c7341990afbc1e7
Roshana Devkota <roshana.devkota@gmail.COM>
2015-11-17 22:32:58 -0500

    added some styling in  each erb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 10 }
   validates :body, presence: true
